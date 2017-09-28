16f77faa58d4e011cf09cf40af2dfab1d9a75e11
ChongFF <fanfei.chong@gmail.com>
2017-09-27 21:47:59 -0400

    Update install-gpu-docker.sh

--- install-gpu-docker.sh
+++ install-gpu-docker.sh
@@ -60,6 +60,16 @@ sudo docker run hello-world
 sudo usermod -a -G docker $USER 
 echo "Added the current user to docker group - Log out and log back in to solve Docker permission error"
 
+# install Docker
+# Step 3) Install nvidia-docker
+# Quick Start Guide here -- https://github.com/NVIDIA/nvidia-docker
+# Install nvidia-docker and nvidia-docker-plugin
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
+sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
+
+# Test nvidia-smi
+nvidia-docker run --rm nvidia/cuda nvidia-smi
+
 # install and configure theano
 pip install theano
 echo "[global]
