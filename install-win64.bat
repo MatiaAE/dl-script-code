03f941cbc29687e9a7d786181a2598e2de4110a4
ChongFF <fanfei.chong@gmail.com>
2017-10-12 18:40:31 -0400

    Update install-win64.bat

--- install-win64.bat
+++ install-win64.bat
@@ -3,7 +3,6 @@ conda install conda-build
 activate tensorflow
 conda install -y numpy matplotlib scipy scikit-learn pillow pandas jupyter
 pip install --ignore-installed --upgrade tensorflow
-conda install -y numpy matplotlib scipy scikit-learn pillow pandas jupyter
 conda install -y mingw libpython
 conda install -y theano
 conda install -y pyyaml HDF5 h5py 
