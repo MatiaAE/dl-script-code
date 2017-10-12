2e37a4b6c0985b78c340192bf6f483a04953ae42
ChongFF <fanfei.chong@gmail.com>
2017-10-12 18:39:12 -0400

    Create install-win64.bat

---
+++ install-win64.bat
@@ -0,0 +1,10 @@
+conda create -n tensorflow
+conda install conda-build
+activate tensorflow
+conda install -y numpy matplotlib scipy scikit-learn pillow pandas jupyter
+pip install --ignore-installed --upgrade tensorflow
+conda install -y numpy matplotlib scipy scikit-learn pillow pandas jupyter
+conda install -y mingw libpython
+conda install -y theano
+conda install -y pyyaml HDF5 h5py 
+pip install keras
