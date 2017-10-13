ee635a41001eef7c6efd23a573a1139ee073e27f
ChongFF <fanfei.chong@gmail.com>
2017-10-12 20:13:29 -0400

    Update install-win64-part2.bat

--- install-win64-part2.bat
+++ install-win64-part2.bat
@@ -1,6 +1,8 @@
 conda install -y numpy matplotlib scipy scikit-learn pillow pandas jupyter
 pip install --ignore-installed --upgrade tensorflow
 conda install -y mingw libpython
-conda install -y theano
+conda install theano=0.8
+REM theano 0.9 does not work in Windows Python 3.5 or 3.6, whether installed by pip or conda
+REM theano 0.8 works, but require Windows Python 3.5, not compatible with Python 3.6
 conda install -y pyyaml HDF5 h5py
 pip install keras
