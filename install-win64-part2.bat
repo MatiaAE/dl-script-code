9abc90a00681303e6aae4751415d17e27c01dd4a
ChongFF <fanfei.chong@gmail.com>
2017-10-12 20:15:26 -0400

    Update install-win64-part2.bat

--- install-win64-part2.bat
+++ install-win64-part2.bat
@@ -6,3 +6,6 @@ REM theano 0.9 does not work in Windows Python 3.5 or 3.6, whether installed by 
 REM theano 0.8 works, but require Windows Python 3.5, not compatible with Python 3.6
 conda install -y pyyaml HDF5 h5py
 pip install keras
+REM pip error reference: http://jakzaprogramowac.pl/pytanie/92058,pip-throws-typeerror-parse-got-an-unexpected-keyword-argument-39-transport-encoding-39-when-trying-to-install-new-packages
+REM Download https://github.com/html5lib/html5lib-python/tree/master/html5lib and overwrite all the files within html5lib folder 
+REM in your tensorflow environment "envs\tensorflow\Lib\site-packages\html5lib" Then you should be able to run any "pip install" commands after that
