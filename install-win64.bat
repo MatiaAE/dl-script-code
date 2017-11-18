039e6530cf9745f9a29067584d331541df207cb2
ChongFF <fanfei.chong@gmail.com>
2017-11-18 12:17:10 -0500

    Update install-win64.bat

--- install-win64.bat
+++ install-win64.bat
@@ -8,7 +8,7 @@ activate tensorflow
 conda install -y numpy matplotlib scipy scikit-learn pillow pandas jupyter
 pip install --ignore-installed --upgrade tensorflow
 conda install -y mingw libpython
-conda install theano=0.8
+conda install -y theano=0.8
 REM theano 0.9 does not work in Windows Python 3.5 or 3.6, whether installed by pip or conda
 REM theano 0.8 works, but require Windows Python 3.5, not compatible with Python 3.6
 conda install -y pyyaml HDF5 h5py
