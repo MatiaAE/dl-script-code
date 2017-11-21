b5a24fa19bec0be779caf2b32a5492785c0afa16
ChongFF <fanfei.chong@gmail.com>
2017-11-21 13:08:23 -0500

    Update install-win64.bat

--- install-win64.bat
+++ install-win64.bat
@@ -2,7 +2,7 @@ ECHO https://ss64.com/nt/start.html - learn to use START / CALL to run sequentia
 conda create -n tensorflow python=3.5 
 ECHO theano 0.9 does not work well under windows, but 0.8* does not support python 3.6
 ECHO 'conda-env remove -n tensorflow' to remove an environment
-conda install conda-build
+conda install -y conda-build
 activate tensorflow
 
 conda install -y numpy matplotlib scipy scikit-learn pillow pandas jupyter
